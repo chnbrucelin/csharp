2d148926391d1fb40dbf805ec8e2d7a75dd73d13
chn.bruce.lin <chn.bruce.lin@foxmail.com>
2012-05-15 17:01:09 +0800

    初始提交
    1) 窗体居中
    2) 宽度*0.9，高度0.8

---
+++ InfoSpider/InfoSpiderForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace InfoSpider
+{
+    public partial class InfoSpiderForm : Form
+    {
+        public int formHeight;
+        public int formWidth;
+
+        public InfoSpiderForm()
+        {
+            InitializeComponent();
+        }
+
+        private void InfoSpiderForm_Load(object sender, EventArgs e)
+        {
+            formHeight = SystemInformation.WorkingArea.Height* 8/10;
+            formWidth = SystemInformation.WorkingArea.Width*9/10;
+            this.ClientSize = new System.Drawing.Size(formWidth, formHeight);
+
+            this.SetBounds((Screen.GetBounds(this).Width / 2) - (this.Width / 2), (Screen.GetBounds(this).Height / 2) - (this.Height / 2), this.Width, this.Height, BoundsSpecified.Location);
+        }
+    }
+}
